c6fa97f6aa8e6ca3100879b2a35aff8cc83eaf2d
Michael J. Shalewski <mshalewski@outlook.com>
2023-04-14 22:59:40 +0000

    added readValue, clearEquation, invertIndex, and solveEquation JS functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
-class Calculator {
+let equation = [];
+
+function readInput(number){
+    equation.push(number);
+    document.getElementById("display").setAttribute("value", equation.join(" "));
+}
+function invertIndex(){
+    let lastIndex = equation.length - 1;
+    if(equation[lastIndex].charAt(0) != '-'){
+        alert("Fuck");
+    }else if(0 === 1){
     
+    }else{
+
+    }
+}
+function clearEquation(){
+    equation = [];
+
+}   
+function solveEquation(){
+    if(eval(equation.join(" ")) !== undefined){
+        document.getElementById("display").setAttribute("value", eval(equation.join(" ")));
+    }else{
+        document.getElementById("display").setAttribute("value", "Error!");
+    }
 }
